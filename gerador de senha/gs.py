a2b8920a42ccf0e63b8e601c1e9e9af02c6a92bf
kevin <kevinup13@gmail.com>
2022-02-04 21:15:13 -0400

    insercao de linha de codigo

--- gerador de senha/gs.py
+++ gerador de senha/gs.py
@@ -3,6 +3,6 @@ import random, string
 tamanho = 16
 chars = string.ascii_letters + string.digits + '!@#ç$%¨&*-_+<>:;/'
 rnd = random.SystemRandom()
-
+print("\nSenha criada:")
 print(''.join(rnd.choice(chars) for i in range (tamanho)))
 
