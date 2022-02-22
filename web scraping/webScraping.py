ef0fb33548602d7d2bf9cd36b80416cb9c41714c
kevin <kevinup13@gmail.com>
2022-02-22 19:58:32 -0400

    alteracao de linha de code

--- web scraping/webScraping.py
+++ web scraping/webScraping.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-site = requests.get("https://www.digitalocean.com/").content
+site = requests.get("https://www.climatempo.com.br/").content
 #objeto site recebendo o conteudo da requisição http do site....
 soup = BeautifulSoup(site, 'html.parser')
 #objeto soup baixando do site o html
