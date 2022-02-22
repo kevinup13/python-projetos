1c8d07e0ac7cd70e400cb3d8330fd0621df9ad37
kevin <kevinup13@gmail.com>
2022-02-22 19:55:35 -0400

    gerador wordlist e web scrapings adicionados

---
+++ web scraping/webScraping.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+site = requests.get("https://www.digitalocean.com/").content
+#objeto site recebendo o conteudo da requisição http do site....
+soup = BeautifulSoup(site, 'html.parser')
+#objeto soup baixando do site o html
+print(soup.prettify())
+#transforma html em string e print vai exibir o html
